92a5f7acf87be99d490580c4d467fe18908b88bb
Julius Wulf <juliuswulf@Julius-Wulfs-computer.local>
2022-03-11 11:28:46 -0800

    test deploy

---
+++ pages/components/icons/IconSettings.js
@@ -0,0 +1,15 @@
+const IconSettings = {
+    size: {
+        small: 12,
+        medium: 16,
+        large: 24,
+        extraLarge: 48,
+    },
+    fill: {
+        white: "#fff",
+        black: "#000",
+        none: "none",
+    },
+}
+
+export default IconSettings;
